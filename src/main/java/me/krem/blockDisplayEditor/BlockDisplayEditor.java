5827374d5b947cabbe3d425d59478d9a2f8e63da
Cec Krem <127088905+Cec-Krem@users.noreply.github.com>
2024-09-08 00:52:09 +0200

    Stable enabling

---
+++ src/main/java/me/krem/blockDisplayEditor/BlockDisplayEditor.java
@@ -0,0 +1,19 @@
+package me.krem.blockDisplayEditor;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BlockDisplayEditor extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("BlockDisplayEditor enabled ");
+        getServer().getPluginManager().registerEvents(new MainListener(this),this);
+        getCommand("bde").setExecutor(new CommandProcessor(this));
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("BlockDisplayEditor disabled");
+    }
+
+}
