7e026de845565b97f92880ab3535dd80fc7e1514
Cec Krem <127088905+Cec-Krem@users.noreply.github.com>
2025-04-05 23:46:58 +0200

    Update BlockDisplayEditor.java

--- src/main/java/me/krem/blockDisplayEditor/BlockDisplayEditor.java
+++ src/main/java/me/krem/blockDisplayEditor/BlockDisplayEditor.java
@@ -2,18 +2,22 @@ package me.krem.blockDisplayEditor;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
 public final class BlockDisplayEditor extends JavaPlugin {
+    private Logger log;
 
     @Override
     public void onEnable() {
-        System.out.println("BlockDisplayEditor enabled");
+        log = getLogger();
+        log.info("BlockDisplayEditor enabled");
         getServer().getPluginManager().registerEvents(new MainListener(this), this);
         getCommand("bde").setExecutor(new CommandProcessor(this));
     }
 
     @Override
     public void onDisable() {
-        System.out.println("BlockDisplayEditor disabled");
+        log.info("BlockDisplayEditor disabled");
     }
 
 }
